3b97f7d66b4598fbb0e0ba1b03fc1c3083067f7b
Alejandro Lizarraga Motta <alejandrolgamotta@hotmail.com>
2023-04-10 17:33:01 -0700

    Avance en CRUD, buscar, mostrar todos, estado, no agrega vacios

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -22,14 +22,115 @@ controller.list = (req,res) =>{
     });
 };
 
-controller.save = (req,res)=>{
+controller.save = (req, res) => {
     const data = req.body;
+  
+    // Verificar si los campos requeridos no están vacíos
+    if (!data.nombre || !data.descripcion || !data.precio || !data.urlImagen) {
+      res.status(400).json({ message: 'Los campos nombre, descripción, precio y url de imagen son requeridos' });
+      return;
+    }
+  
+    req.getConnection((err, conn) => {
+      conn.query('INSERT INTO productos SET ?', [data], (err, productos) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ message: 'Error interno del servidor' });
+          return;
+        }
+  
+        res.redirect('/productos');
+      });
+    });
+  };
+  
+
+controller.delete = (req,res)=>{
+    const {id} = req.params;
     req.getConnection((err,conn)=>{
-        conn.query('INSERT INTO productos set ?',[data], (err,producto)=>{
-            console.log(producto);
-            res.send('worksssss');
+        conn.query('DELETE FROM productos WHERE id = ?',[id], (err, rows)=>{
+            res.redirect('/productos');
+        })
+    })
+};
+
+controller.edit = (req,res)=>{
+    const {id} = req.params;
+    req.getConnection((err,conn)=>{
+        conn.query('SELECT * FROM productos WHERE id = ?',[id], (err,productos)=>{
+            res.render('editarProductos',{
+            data: productos[0]
+        });
         });
     });
 };
+
+controller.update = (req,res)=>{
+    const {id} = req.params;
+    const nuevoProducto = req.body;
+    req.getConnection((err,conn)=>{
+        conn.query('UPDATE productos set ? WHERE id = ?',[nuevoProducto, id], (err,rows)=>{
+            res.redirect('/productos');
+        });
+        });
+};
+
+controller.buscar = (req, res) => {
+    const { q } = req.query;
+    req.getConnection((err, conn) => {
+      conn.query(
+        'SELECT * FROM productos WHERE id LIKE ?',
+        [`%${q}%`],
+        (err, productos) => {
+          if (err) {
+            res.json(err);
+          }
+          res.render('productos', {
+            data: productos,
+          });
+        }
+      );
+    });
+  };
+
+  controller.estado = (req, res) => {
+    const { id } = req.params;
+  
+    req.getConnection((err, conn) => {
+      conn.query('SELECT estado FROM productos WHERE id = ?', [id], (err, rows) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ message: 'Error interno del servidor' });
+          return;
+        }
+  
+        if (rows.length === 0) {
+          res.status(404).json({ message: 'Producto no encontrado' });
+          return;
+        }
+  
+        const estadoActual = rows[0].estado;
+  
+        let nuevoEstado;
+        if (estadoActual === 'activado') {
+          nuevoEstado = 'desactivado';
+        } else {
+          nuevoEstado = 'activado';
+        }
+  
+        conn.query('UPDATE productos SET estado = ? WHERE id = ?', [nuevoEstado, id], (err, rows) => {
+          if (err) {
+            console.log(err);
+            res.status(500).json({ message: 'Error interno del servidor' });
+            return;
+          }
+  
+          res.redirect('/productos');
+        });
+      });
+    });
+  };
+  
+  
 
 module.exports = controller;
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -5,5 +5,10 @@ const router = express.Router();
 const productosController = require('../controllers/productosController');
 
 router.get('/productos', productosController.list);
-router.post('/add', productosController.save)
+router.post('/add', productosController.save);
+router.get('/delete/:id', productosController.delete);
+router.get('/update/:id', productosController.edit);
+router.post('/update/:id', productosController.update);
+router.get('/buscar', productosController.buscar);
+router.get('/estado/:id', productosController.estado);
 module.exports = router;
