fc54e70dfd272bbfeb0603521f9fb37244a3d360
Alejandro Lizarraga Motta <alejandrolgamotta@hotmail.com>
2023-04-18 11:37:30 -0700

    final destination

--- src/bd.js
+++ src/bd.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql');
-// const pool = mysql.createPool({
-//     connectionLimit : 20,
-//     host: '52.32.208.197',
-//     user:   'pepito',
-//     password: 'pepito',
-//     database: 'mtt',
-// });
-
 const pool = mysql.createPool({
     connectionLimit : 20,
-    host: 'localhost',
-    user:   'root',
-    password: '',
+    host: '52.32.208.197',
+    user:   'pepito',
+    password: 'pepito',
     database: 'mtt',
 });
+
+// const pool = mysql.createPool({
+//     connectionLimit : 20,
+//     host: 'localhost',
+//     user:   'root',
+//     password: '',
+//     database: 'mtt',
+// });
 
 module.exports = pool;
